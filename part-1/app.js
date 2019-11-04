371c1739ff64da3218e2d3cb1b73f620e8b125ac
nasenatalie21 <nasenatalie@gmail.com>
2019-11-04 16:11:54 +0800

    Add files via upload

--- part-1/app.js
+++ part-1/app.js
@@ -1,4 +1,3 @@
-  
 // Set constraints for the video stream
 var constraints = {video: {facingMode:{exact: 'user'}}, audio: false};
 var constraints2 = {video: {facingMode:{exact: 'environment'}}, audio: false};
@@ -61,52 +60,23 @@ cameraTrigger.onclick = function() {
     var image2 = new Image();
     image2 = document.getElementById('overlay');
     image2.crossOrigin = "Anonymous";
-    // cameraSensor.width = image2.width;
-    // cameraSensor.height = image2.height;
 
     var canvas = document.getElementById('camera--sensor'); 
     var context = canvas.getContext('2d');  
-
-    // context.globalAlpha = 1.0;
-    context.drawImage(cameraView, 0, 0, cameraSensor.width, cameraSensor.height);
+    context.drawImage(cameraView, 0, -20, cameraSensor.width, cameraSensor.height);
     image2.src = "frame.png";
-    // context.globalAlpha = 0.5;
-    context.drawImage(image2, 0, -10, cameraSensor.width, cameraSensor.height);
+    context.drawImage(image2, 0, -20, cameraSensor.width, cameraSensor.height);
 
     // context.globalAlpha = 1.0;
     // context.drawImage(cameraView, 0, 0, 328, 526);
     // context.globalAlpha = 0.5; //Remove if pngs have alpha
     // context.drawImage(image2, 15, 85, 300, 300);
 
-    // cameraSensor.width = overlay.width;
-    // cameraSensor.height = overlay.height;
-
-    // var canvas = document.getElementById('camera--sensor');  
-    // var context = canvas.getContext('2d');  
-    // context.drawImage(cameraView, 0, 0);
     // cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    // var canvas = document.getElementById("camera--sensor");
-    // var ctx = canvas.getContext("2d");
-    // var img = document.getElementById("overlay");
-    // ctx.drawImage(img, 10, 10);
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
-    // console.log(cameraOutput.src);
-    // cameraOutput.src = image2;
     cameraOutput.classList.add("taken");
     // track.stop();
 };
-
-// var image1 = new Image();
-// image1 = cameraOutput.src;
-// image1.onload = function() {
-//     cameraSensor.getContext("2d").drawImage(image1, 0, 0);
-// };
-
-// var image2 = new Image();
-// image2.src = "./frame.png";
-// image2.onload = function() {
-//     cameraSensor.getContext("2d").drawImage(image2, 0, 0);
-// };
 
 // Save the image to local gallery
 saveImage.onclick = function() {
